17ed937f17db2a3f3f46327f6060ce913d4845a8
T7Q <tkuumola@gmail.com>
2020-06-11 12:03:01 +0300

    build: :arrow_up: install more plugins
    Minimize CSS, Optimize CSS Assets, terser plugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,15 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     output: {
         libraryTarget:'var',
         library: 'Client'
@@ -19,7 +24,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
